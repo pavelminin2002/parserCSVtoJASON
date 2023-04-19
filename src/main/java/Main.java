477927184a00e6a52c0b27831ed0ff4d66f5bfbc
Pavel Minin <minin09092002@mail.ru>
2023-04-20 01:11:12 +0300

    Task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,13 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,8 +26,10 @@ public class Main {
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json, "data.json");
+        list = parseXML("data.xml");
+        json = listToJson(list);
+        writeString(json, "data2.json");
     }
-
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
         ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
@@ -57,5 +65,43 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> result = new ArrayList<>();
+        List<String> parameters = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(fileName));
+
+            Node root = document.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node child = nodeList.item(i);
+                if (Node.ELEMENT_NODE == child.getNodeType() && child.getNodeName().equals("employee")) {
+                    NodeList childs = child.getChildNodes();
+                    for (int j = 0; j < childs.getLength(); j++) {
+                        if (childs.item(j).getNodeType() == Node.ELEMENT_NODE) {
+                            parameters.add(childs.item(j).getTextContent());
+                        }
+                    }
+
+                    Employee employee = new Employee(
+                            Long.parseLong(parameters.get(0)),
+                            parameters.get(1),
+                            parameters.get(2),
+                            parameters.get(3),
+                            Integer.parseInt(parameters.get(4))
+                    );
+                    result.add(employee);
+                    parameters.clear();
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
